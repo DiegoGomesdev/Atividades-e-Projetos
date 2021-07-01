c24fd1b49461a62a072f592025df3365ee1ecdd2
Diego Gomes <digomesrique@hotmail.com>
2021-07-01 15:52:30 -0300

    Projeto de atividade simulando dados de um cliente de banco

---
+++ Conta Bancaria/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+namespace Conta_Bancaria
+{
+    class ContaBancaria
+    {
+        private int _numero;
+        private string _titular;
+        private double _saldo;
+
+        public ContaBancaria()
+        {
+
+        }
+     
+        public void GetNumero(int numero)
+        {
+            _numero = numero;
+        }
+        public void GetTitular(string titular)
+        {
+            _titular = titular;
+        }
+        public void GetSaldo(double saldo)
+        {
+            _saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            _saldo = _saldo + quantia;
+        }
+               
+        public void Saque(double quantia)
+        {
+            _saldo = _saldo - quantia - 5.0;
+
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                +_numero 
+                + ", " 
+                + "Titular: " 
+                + _titular + ", " 
+                + "Saldo: $ " 
+                + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Conta Bancaria/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conta_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cliente = new ContaBancaria();
+
+            Console.Write(" Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            cliente.GetNumero(numeroConta);
+           
+            Console.Write("Entre o titular da conta: ");
+            string titularConta = Console.ReadLine();
+            cliente.GetTitular(titularConta);
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcao = char.Parse(Console.ReadLine());
+            double saldo = 0.0;
+            if (opcao == 's' )
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cliente.Deposito(saldo);
+            }
+            else
+            {
+                Console.WriteLine("Nenhum deposito informado! ");
+            }
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(cliente);
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.Deposito(saldo);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+            
+            Console.WriteLine("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+
+
+            Console.ReadLine();
+        }
+    }
+}
